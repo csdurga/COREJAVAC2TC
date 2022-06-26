22df1452da40f9642b6fa36a2ffd501721ff5390
nvenk <nvenk@DESKTOP-768F5B8>
2022-06-26 16:47:02 +0530

    output:The value of x is 3

---
+++ demo1/src/switchcase.java
@@ -0,0 +1,31 @@
+
+public class switchcase {
+	
+		public static void main(String[] args) 
+		{
+						  System.out.println("The value of x is 3"); } 
+		{
+				  {
+			System.out.println("The value of x is other than 1,2,3"); 
+			  }
+			 
+			
+			switch ('x') 
+			{
+			case 1: 
+				System.out.println("The value of x is 1");
+				break;
+			case 2:
+				System.out.println("The value of x is 2");
+				break;
+			case 3:
+				System.out.println("The value of x is 3");
+				break;
+			default:
+			System.out.println("The value of x is other than 1,2,3");
+				break;
+
+			}
+
+}
+}
