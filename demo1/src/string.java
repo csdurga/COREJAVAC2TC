59a0624aee6ae5db88aca6e72f8b469c1c4b2624
nvenk <nvenk@DESKTOP-768F5B8>
2022-06-26 15:22:39 +0530

    output:Hello World

---
+++ demo1/src/string.java
@@ -0,0 +1,12 @@
+
+public class string {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String var = new String("Hello world");
+		System.out.println(var);
+		
+
+	}
+
+}
