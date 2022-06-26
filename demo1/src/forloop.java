fa46e201b042a4fa883cbeed36ec2dedfa09e806
nvenk <nvenk@DESKTOP-768F5B8>
2022-06-26 17:16:18 +0530

    for loop

---
+++ demo1/src/forloop.java
@@ -0,0 +1,15 @@
+
+public class forloop {
+
+	public static void main(String[] args) 
+	{
+		for(int i = 1; i<=1000 ; i++)   
+	{	
+		System.out.print("Value of i:");
+		System.out.println(i);
+	}
+
+
+	}
+
+}
