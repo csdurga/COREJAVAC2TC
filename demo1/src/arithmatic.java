a5017ff23185d69a046eb8c0707cdb95f1c37d79
nvenk <nvenk@DESKTOP-768F5B8>
2022-06-26 14:53:48 +0530

    operators

--- demo1/src/arithmatic.java
+++ demo1/src/arithmatic.java
@@ -3,6 +3,10 @@ public class arithmatic {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int x = 10, y=20;
+		System.out.println(x+y); //30
+		System.out.println(--x); // 9
+		System.out.println(++y); //21
 
 	}
 
