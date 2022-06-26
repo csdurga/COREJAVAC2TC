1632100955c63f08c1e32a150b52d46d693c28f2
nvenk <nvenk@DESKTOP-768F5B8>
2022-06-26 16:06:08 +0530

    output:0

---
+++ demo1/src/Ternaryoperator.java
@@ -0,0 +1,14 @@
+
+public class Ternaryoperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x = 11;
+		
+		x = (10 == x) ? 1 : 0;   
+		
+		System.out.println(x);
+
+	}
+
+}
